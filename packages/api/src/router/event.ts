711e53283b8ebb99c188a3c01f3bbb39a4c3081f
hamtsu <itseatham@gmail.com>
2026-03-13 21:35:13 +1100

    add: event api routes

--- packages/api/src/router/event.ts
+++ packages/api/src/router/event.ts
@@ -1,14 +1,19 @@
 import type { TRPCRouterRecord } from "@trpc/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod/v4";
 
 import { protectedProcedure } from "../trpc";
 
 export const eventRouter = {
-  all: protectedProcedure.query(({ ctx }) => {
-    return ctx.db.event.findMany();
+  // --------------getEvents-----------------------------------------
+  getEvents: protectedProcedure.query(({ ctx }) => {
+    return ctx.db.event.findMany({
+      take: 15,
+    });
   }),
 
-  byId: protectedProcedure
+  // --------------getById-----------------------------------------
+  getById: protectedProcedure
     .input(z.object({ id: z.string() }))
     .query(({ ctx, input }) => {
       return ctx.db.event.findUnique({
@@ -16,7 +21,118 @@ export const eventRouter = {
       });
     }),
 
-  delete: protectedProcedure.input(z.string()).mutation(({ ctx, input }) => {
-    return ctx.db.post.delete({ where: { id: input } });
-  }),
+  // --------------updateById-----------------------------------------
+  updateById: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        data: z.object({
+          name: z.string().optional(),
+          date: z.string().optional(),
+          location: z.string().optional(),
+          content: z.string().optional(),
+        }),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const event = await ctx.db.event.findUnique({
+        where: {
+          id: input.id,
+        },
+        select: {
+          id: true,
+        },
+      });
+
+      if (!event) return {};
+
+      const organiserEvent = await ctx.db.event.findFirst({
+        where: {
+          id: input.id,
+          organisers: {
+            some: {
+              id: ctx.session.user.id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+
+      if (!organiserEvent) {
+        throw new TRPCError({ code: "FORBIDDEN" });
+      }
+
+      return ctx.db.event.update({
+        where: { id: input.id },
+        data: input.data,
+      });
+    }),
+
+  // --------------joinById-----------------------------------------
+  joinById: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.event.update({
+        where: { id: input.id },
+        data: {
+          participants: {
+            connect: {
+              id: ctx.session.user.id,
+            },
+          },
+        },
+      });
+    }),
+  // --------------leaveById-----------------------------------------
+  leaveById: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.event.update({
+        where: { id: input.id },
+        data: {
+          participants: {
+            disconnect: {
+              id: ctx.session.user.id,
+            },
+          },
+        },
+      });
+    }),
+  // --------------deleteById-----------------------------------------
+  deleteById: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      const event = await ctx.db.event.findUnique({
+        where: {
+          id: input,
+        },
+        select: {
+          id: true,
+        },
+      });
+
+      if (!event) return {};
+
+      const organiserEvent = await ctx.db.event.findFirst({
+        where: {
+          id: input,
+          organisers: {
+            some: {
+              id: ctx.session.user.id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+
+      if (!organiserEvent) {
+        throw new TRPCError({ code: "FORBIDDEN" });
+      }
+
+      return ctx.db.event.delete({ where: { id: input } });
+    }),
 } satisfies TRPCRouterRecord;
